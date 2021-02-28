f274c18c9a9c5241eb38d828460bc087124ab454
Evelynn Gresbach <egresbach1@my.wctc.edu>
2021-02-27 20:30:16 -0600

    Create TicketFile class for file IO

---
+++ TicketFile.cs
@@ -0,0 +1,11 @@
+using System; 
+using System.Collections.Generic;
+
+class TicketFile{
+    public string file {get; set;}
+    public List<Ticket> Tickets {get; set;}
+
+    //Constructor to reads from file
+    //Method to write to file
+    //Method to get unique id
+}
