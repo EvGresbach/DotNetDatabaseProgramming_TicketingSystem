b5db1273528482a897f6be0aab8aebe413f1768f
Evelynn Gresbach <egresbach1@my.wctc.edu>
2021-03-08 13:50:08 -0600

    Ask user type of ticket

--- Program.cs
+++ Program.cs
@@ -10,75 +10,42 @@ namespace TicketingSystem
         {
             NLog.Logger logger = NLog.Web.NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
     
-            // string file = "Tickets.csv";
-            TicketFile ticketFile = new TicketFile("Tickets.csv"); 
+            // create ticket files for each type
+            TicketFile defectives = new TicketFile("Tickets.csv", "Defect"); 
+            TicketFile enhancements = new TicketFile("Enhancements.csv", "Enhancement");
+            TicketFile tasks = new TicketFile("Task.csv", "Task"); 
+
+
             string userChoice; 
             do {
-                Console.WriteLine("1. Read data from file\n2. Add data to file \nEnter any key to exit");
+                Console.Write("1. Read data from file\n2. Add data to file \nEnter any key to exit\n>");
                 userChoice = Console.ReadLine(); 
                 // choice 1 - read data
                 if (userChoice == "1")
                 {
-                    foreach(Ticket t in ticketFile.Tickets){
-                        Console.WriteLine(t + "\n"); 
-                    }
-                    // if (File.Exists(file))
-                    // {
-                    //     StreamReader sr = new StreamReader(file);
-                        
-                    //     while (!sr.EndOfStream)
-                    //     {
-                    //         string line = sr.ReadLine();
-                    //         string[] ticket = line.Split(',');
-                    //         string[] watcher = ticket[6].Split('|');
-                    //         Console.WriteLine($"Ticket {ticket[0]} \n----------------------------");
-                    //         Console.WriteLine($"Summary: {ticket[1]}\nStatus: {ticket[2]} \nPriority: {ticket[3]}\n"
-                    //             + $"Submitter: {ticket[4]} \nAssigned: {ticket[5]} \nWatching: {String.Join(", ", watcher)}\n\n");
-                    //     }
-                    //     sr.Close(); 
-                    // }
-                    // else Console.WriteLine("This file does not exist");
+                    //ask what file to read from 
+                    Console.Write("Which type of ticket do you wish to look at?\n1. Bugs/Defects\n2. Enhancements\n3. Tasks\n>"); 
+                    int toReadFrom; 
+                    while(!Int32.TryParse(Console.ReadLine(), out toReadFrom)){
+                        Console.Write("Answer must be an integer\n1. Bugs/Defects\n2. Enhancements\n3. Tasks\n>");
+                    } 
+
+                    //read from that file
+                    
+                    
                 }
                 // choice 2 - add data
                 else if (userChoice == "2")
                 {
-                    // StreamWriter sw = new StreamWriter(file);
-
-                    Console.Write("Enter Ticket ID: "); 
-                    int ticketID; 
-                    if(!Int32.TryParse(Console.ReadLine(), out ticketID)){
-                        Console.Write("ID must be an integer. Please try again: ");
+                   //ask what file to write to
+                   Console.Write("Which type of ticket do you wish to write?\n1. Bug/Defect\n2. Enhancement\n3. Task\n>"); 
+                    int toWrite; 
+                    while(!Int32.TryParse(Console.ReadLine(), out toWrite){
+                        Console.Write("Answer must be an integer\n1. Bugs/Defect\n2. Enhancement\n3. Task\n>");
                     } 
-                    if(!ticketFile.IsUnique(ticketID)){
-                        logger.Info("Invalid ID " + ticketID);  
-                        break; 
-                    }
-                    Console.Write("Enter Summary: "); 
-                    string summary = Console.ReadLine(); 
-                    Console.Write("Enter Status: "); 
-                    string status = Console.ReadLine(); 
-                    Console.Write("Enter Priority: "); 
-                    string priority = Console.ReadLine(); 
-                    Console.Write("Enter Submitter: "); 
-                    string submitter = Console.ReadLine(); 
-                    Console.Write("Enter Assigned: "); 
-                    string assigned = Console.ReadLine(); 
-                    
-                    string moreWatching;
-                    List<string> watching = new List<string>(); 
 
-                    do {
-                        Console.Write("Enter Watching: "); 
-                        watching.Add(Console.ReadLine()); 
-
-                        Console.Write("Enter another person watching? Y/N: ");
-                        moreWatching = Console.ReadLine().ToUpper(); 
-                    } while(moreWatching == "Y");
-                    Ticket ticket = new Ticket(ticketID, summary, status, priority, submitter, assigned, watching); 
-                    // sw.WriteLine($"{ticketID},{summary},{status},{priority},{submitter},{assigned},{String.Join("|", watching)}"); 
-                    // sw.Close();
-
-                    ticketFile.AddTicket(ticket); 
+                   //collect information 
+                   //add ticket to ticketfile list 
                 }
             } while (userChoice == "1" || userChoice == "2");
         }
