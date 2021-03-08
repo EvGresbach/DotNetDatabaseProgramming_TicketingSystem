ad5db781aceb049b49d10b4436e3f622036b580b
Evelynn Gresbach <egresbach1@my.wctc.edu>
2021-03-08 12:42:16 -0600

    Change Ticket class to abstract

--- Ticket.cs
+++ Ticket.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic; 
 
 namespace TicketingSystem{
-    class Ticket{  
+    public abstract class Ticket{  
         public int id {get; set;}
         public string summary {get; set;}
         public string status {get; set;}
@@ -12,25 +12,37 @@ namespace TicketingSystem{
         public List<string> watching {get; set;} 
 
         //constructor
-        public Ticket(int id, string summary, string status, string priority, string submitter, string assigned, List<String> watching){
-            this.id = id; 
-            this.summary = summary; 
-            this.status = status; 
-            this.priority = priority; 
-            this.submitter = submitter; 
-            this.assigned = assigned; 
-            this.watching = watching; 
-        }
+        // public Ticket(int id, string summary, string status, string priority, string submitter, string assigned, List<String> watching){
+        //     this.id = id; 
+        //     this.summary = summary; 
+        //     this.status = status; 
+        //     this.priority = priority; 
+        //     this.submitter = submitter; 
+        //     this.assigned = assigned; 
+        //     this.watching = watching; 
+        // }
 
         public Ticket(){
             watching = new List<string>(); 
         }
 
         //method to print 
-        public override string ToString(){
+        public virtual string Display(){
             return String.Format($"Ticket {id}:\nSummary: {summary}\nPriority: {priority}\nSubmitter: {submitter}\nAssigned: {assigned}\nWatching: {String.Join(", ", watching)}"); 
         }
-    }  
+    } 
+
+    public class Defect : Ticket {
+        
+    }
+
+    public class Enhancement : Ticket {
+
+    }
+
+    public class Task : Ticket {
+
+    }
 }
 
 
